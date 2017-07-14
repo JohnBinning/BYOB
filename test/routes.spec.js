086575b3b75a7b5b4171e4afdf5b176fb1a52399
John Binning <john.binning17@gmail.com>
2017-07-14 11:59:25 -0600

    update test with before migration

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -16,7 +16,19 @@ const should = chai.should();
 chai.use(chaiHttp);
 
 const authToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InN0ZXZlIiwicGFzc3dvcmQiOiJ0aWdlciIsImlhdCI6MTUwMDAwNDYzNywiZXhwIjoxNTAxMjE0MjM3fQ.ZQtF1M9Rfq6e1pAK4C11g5Joo9fVRLYOG6_StgbLA-Q';
+
 describe('base sad path', () => {
+
+  before(done => {
+    database.migrate.latest()
+      .then(() => {
+        return database.seed.run()
+        .then(() => {
+          done();
+        });
+    });
+  });
+
   beforeEach((done) => {
     database.seed.run()
       .then(() => {
@@ -44,6 +56,17 @@ describe('base sad path', () => {
 });
 
 describe('franchise routes', () => {
+
+  before(done => {
+    database.migrate.latest()
+      .then(() => {
+        return database.seed.run()
+        .then(() => {
+          done();
+        });
+    });
+  });
+
   beforeEach((done) => {
     database.seed.run()
       .then(() => {
@@ -235,6 +258,17 @@ describe('franchise routes', () => {
 });
 
 describe('inducted people routes', () => {
+
+  before(done => {
+    database.migrate.latest()
+      .then(() => {
+        return database.seed.run()
+        .then(() => {
+          done();
+        });
+    });
+  });
+
   beforeEach((done) => {
     database.seed.run()
       .then(() => {
@@ -454,26 +488,122 @@ describe('inducted people routes', () => {
   });
 
 describe('batter_data routes', () => {
-    beforeEach((done) => {
-      database.seed.run()
+
+  before(done => {
+    database.migrate.latest()
+      .then(() => {
+        return database.seed.run()
         .then(() => {
           done();
         });
     });
+  });
 
-    it('should get the batters', (done) => {
-      chai.request(server)
-      .get('/api/v1/batter_data')
-      .end((err, response) => {
-        response.should.have.status(200);
-        response.should.be.json;
-        response.body[0].should.be.a('object');
-        response.body.length.should.equal(12);
+  beforeEach((done) => {
+    database.seed.run()
+      .then(() => {
         done();
       });
-    });
+  });
 
-    it('should get a specific batter', (done) => {
+  it('should get the batters', (done) => {
+    chai.request(server)
+    .get('/api/v1/batter_data')
+    .end((err, response) => {
+      response.should.have.status(200);
+      response.should.be.json;
+      response.body[0].should.be.a('object');
+      response.body.length.should.equal(12);
+      done();
+    });
+  });
+
+  it('should get a specific batter', (done) => {
+    chai.request(server)
+    .get('/api/v1/batter_data/id/1')
+    .end((err, response) => {
+
+      response.should.have.status(200);
+      response.should.be.json;
+      response.body[0].should.be.a('object');
+      response.body.length.should.equal(1);
+      done();
+    });
+  });
+
+  it('should not get a specific batter with bad id info', (done) => {
+    chai.request(server)
+    .get('/api/v1/batter_data/id/10000')
+    .end((err, response) => {
+
+      response.should.have.status(404);
+      response.body.should.be.a('object');
+      response.body.error.should.equal('No batters found with id of 10000')
+      done();
+    });
+  });
+
+  it('should post a batter', (done) => {
+
+    let newPersonBody = {
+    "newPerson": {
+      "id": 29,
+  		"career": "1984-2017",
+  		"induction_method": "BBWAA",
+  		"name": "Roy Hobbs",
+  		"primary_team": "Atlanta Braves",
+  		"vote_percentage": "88",
+  		"year_inducted": "2017",
+  		"team_id": "4"
+  	},
+    	token: authToken
+  }
+  chai.request(server)
+  .post('/api/v1/inducted_people')
+  .send(newPersonBody)
+  .end((err, response) => {
+    response.should.have.status(201);
+    response.body.id.should.equal(29);
+    let newBatterBody = {
+      "newBatter": {
+        "id": 13,
+        "avg": ".350",
+        "hits": "140",
+        "hr": "44",
+        "name": "Roy Hobbs",
+        "induction_id": 1 ,
+        "obp": ".447",
+        "rbi": "106",
+        "runs": "92",
+        "sb": "3",
+        "slg": ".750"
+      },
+        token: authToken
+    }
+    chai.request(server)
+    .post('/api/v1/batter_data')
+    .send(newBatterBody)
+    .end((err, response) => {
+      response.should.have.status(201);
+      response.body.id.should.equal(13)
+      done();
+    });
+  });
+
+  it('should edit a batter', (done) => {
+    let newUpdate = {
+      "batter": {
+        "name": "Roy Hobbs"
+        },
+        token: authToken
+    }
+    chai.request(server)
+    .put('/api/v1/batter_data/id/1')
+    .send(newUpdate)
+    .end((err, response) => {
+
+      response.should.have.status(201);
+
       chai.request(server)
       .get('/api/v1/batter_data/id/1')
       .end((err, response) => {
@@ -481,112 +611,27 @@ describe('batter_data routes', () => {
         response.should.have.status(200);
         response.should.be.json;
         response.body[0].should.be.a('object');
-        response.body.length.should.equal(1);
-        done();
+        response.body[0].name.should.equal('Roy Hobbs');
+      done();
       });
     });
+  });
 
-    it('should not get a specific batter with bad id info', (done) => {
-      chai.request(server)
-      .get('/api/v1/batter_data/id/10000')
-      .end((err, response) => {
-
-        response.should.have.status(404);
-        response.body.should.be.a('object');
-        response.body.error.should.equal('No batters found with id of 10000')
-        done();
-      });
-    });
-
-    it('should post a batter', (done) => {
-
-      let newPersonBody = {
-      "newPerson": {
-        "id": 29,
-    		"career": "1984-2017",
-    		"induction_method": "BBWAA",
-    		"name": "Roy Hobbs",
-    		"primary_team": "Atlanta Braves",
-    		"vote_percentage": "88",
-    		"year_inducted": "2017",
-    		"team_id": "4"
-    	},
-      	token: authToken
+  it('should not edit a batter if the id does not exist', (done) => {
+    let newUpdate = {
+      "batter": {
+        "name": "Roy Hobbs"
+        },
+        token: authToken
     }
     chai.request(server)
-    .post('/api/v1/inducted_people')
-    .send(newPersonBody)
+    .put('/api/v1/batter_data/id/100000')
+    .send(newUpdate)
     .end((err, response) => {
-      response.should.have.status(201);
-      response.body.id.should.equal(29);
-      let newBatterBody = {
-        "newBatter": {
-          "id": 13,
-          "avg": ".350",
-          "hits": "140",
-          "hr": "44",
-          "name": "Roy Hobbs",
-          "induction_id": 1 ,
-          "obp": ".447",
-          "rbi": "106",
-          "runs": "92",
-          "sb": "3",
-          "slg": ".750"
-        },
-          token: authToken
-      }
-      chai.request(server)
-      .post('/api/v1/batter_data')
-      .send(newBatterBody)
-      .end((err, response) => {
-        response.should.have.status(201);
-        response.body.id.should.equal(13)
-        done();
+
+      response.should.have.status(422);
+      done();
       });
-    });
-
-    it('should edit a batter', (done) => {
-      let newUpdate = {
-        "batter": {
-          "name": "Roy Hobbs"
-          },
-          token: authToken
-      }
-      chai.request(server)
-      .put('/api/v1/batter_data/id/1')
-      .send(newUpdate)
-      .end((err, response) => {
-
-        response.should.have.status(201);
-
-        chai.request(server)
-        .get('/api/v1/batter_data/id/1')
-        .end((err, response) => {
-
-          response.should.have.status(200);
-          response.should.be.json;
-          response.body[0].should.be.a('object');
-          response.body[0].name.should.equal('Roy Hobbs');
-        done();
-        });
-      });
-    });
-
-    it('should not edit a batter if the id does not exist', (done) => {
-      let newUpdate = {
-        "batter": {
-          "name": "Roy Hobbs"
-          },
-          token: authToken
-      }
-      chai.request(server)
-      .put('/api/v1/batter_data/id/100000')
-      .send(newUpdate)
-      .end((err, response) => {
-
-        response.should.have.status(422);
-        done();
-        });
     });
   });
 
@@ -617,80 +662,174 @@ describe('batter_data routes', () => {
       });
     });
 
-    it('should not edit a batter if the id does not exist', (done) => {
-      let newUpdate = {
-        "batter": {
-          "name": "Roy Hobbs"
-          },
-          token: authToken
-      }
+  it('should not edit a batter if the id does not exist', (done) => {
+    let newUpdate = {
+      "batter": {
+        "name": "Roy Hobbs"
+        },
+        token: authToken
+    }
+    chai.request(server)
+    .put('/api/v1/batter_data/id/100000')
+    .send(newUpdate)
+    .end((err, response) => {
+
+      response.should.have.status(422);
+      done();
+      });
+  });
+
+  it('should delete a batter', (done) => {
+    chai.request(server)
+    .get('/api/v1/batter_data')
+    .end((err, response) => {
+
+      response.body.length.should.equal(12);
+
       chai.request(server)
-      .put('/api/v1/batter_data/id/100000')
-      .send(newUpdate)
-      .end((err, response) => {
-
-        response.should.have.status(422);
-        done();
-        });
-    });
-
-    it('should delete a batter', (done) => {
-      chai.request(server)
-      .get('/api/v1/batter_data')
-      .end((err, response) => {
-
-        response.body.length.should.equal(12);
+      .delete('/api/v1/batter_data/delete/1')
+      .send({ token: authToken })
+      .end((error, response) => {
+        response.should.have.status(204);
 
         chai.request(server)
-        .delete('/api/v1/batter_data/delete/1')
-        .send({ token: authToken })
-        .end((error, response) => {
-          response.should.have.status(204);
+        .get('/api/v1/batter_data')
+        .end((err, response) => {
 
-          chai.request(server)
-          .get('/api/v1/batter_data')
-          .end((err, response) => {
-
-            response.body.length.should.equal(11);
-          })
-        done();
-        });
+          response.body.length.should.equal(11);
+        })
+      done();
       });
     });
+  });
 
-    it('should not delete a batter w/o a correct token', (done) => {
-      chai.request(server)
-        .delete('/api/v1/batter_data/delete/1')
-        .send({ token: '42' })
-        .end((error, response) => {
-          response.should.have.status(403);
-        done()
-      })
-    });
+  it('should not delete a batter w/o a correct token', (done) => {
+    chai.request(server)
+      .delete('/api/v1/batter_data/delete/1')
+      .send({ token: '42' })
+      .end((error, response) => {
+        response.should.have.status(403);
+      done()
+    })
+  });
 
 });
 
 describe('pitcher_data routes', () => {
-    beforeEach((done) => {
-      database.seed.run()
+
+  before(done => {
+    database.migrate.latest()
+      .then(() => {
+        return database.seed.run()
         .then(() => {
           done();
         });
     });
+  });
 
-    it('should get the pitchers', (done) => {
+  beforeEach((done) => {
+    database.seed.run()
+      .then(() => {
+        done();
+      });
+  });
+
+  it('should get the pitchers', (done) => {
+    chai.request(server)
+    .get('/api/v1/pitcher_data')
+    .end((err, response) => {
+      response.should.have.status(200);
+      response.should.be.json;
+      response.body[0].should.be.a('object');
+      response.body.length.should.equal(10);
+      done();
+    });
+  });
+
+  it('should get a specific pitcher', (done) => {
+    chai.request(server)
+    .get('/api/v1/pitcher_data/id/1')
+    .end((err, response) => {
+
+      response.should.have.status(200);
+      response.should.be.json;
+      response.body[0].should.be.a('object');
+      response.body.length.should.equal(1);
+      done();
+    });
+  });
+
+  it('should not get a specific pitcher with bad id info', (done) => {
+    chai.request(server)
+    .get('/api/v1/pitcher_data/id/10000')
+    .end((err, response) => {
+
+      response.should.have.status(404);
+      response.body.should.be.a('object');
+      response.body.error.should.equal('No pitchers found with id of 10000')
+      done();
+    });
+  });
+
+  it('should post a pitcher', (done) => {
+
+    let newPersonBody = {
+      "newPerson": {
+        "id": 29,
+        "career": "1984-2017",
+        "induction_method": "BBWAA",
+        "name": "Ricky Vaughn",
+        "primary_team": "Atlanta Braves",
+        "vote_percentage": "88",
+        "year_inducted": "2017",
+        "team_id": "4"
+      },
+        token: authToken
+    }
+    chai.request(server)
+    .post('/api/v1/inducted_people')
+    .send(newPersonBody)
+    .end((err, response) => {
+      let newPitcherBody = {
+        "newPitcher": {
+          "id": 11,
+          "name": "Ricky Vaughn",
+          "games": "189",
+          "starts": "103",
+          "wins": "46",
+          "losses": "40",
+          "era": "3.76",
+          "strikeouts": "613",
+          "walks": "325",
+          "induction_id": 29
+        },
+          token: authToken
+      }
       chai.request(server)
-      .get('/api/v1/pitcher_data')
+      .post('/api/v1/pitcher_data')
+      .send(newPitcherBody)
       .end((err, response) => {
-        response.should.have.status(200);
-        response.should.be.json;
-        response.body[0].should.be.a('object');
-        response.body.length.should.equal(10);
+        response.should.have.status(201);
+        response.body.id.should.equal(11)
         done();
       });
     });
+  });
 
-    it('should get a specific pitcher', (done) => {
+  it('should edit a pitcher', (done) => {
+    let newUpdate = {
+      "pitcher": {
+        "name": "Roy Hobbs"
+        },
+        token: authToken
+    }
+    chai.request(server)
+    .put('/api/v1/pitcher_data/id/1')
+    .send(newUpdate)
+    .end((err, response) => {
+
+      response.should.have.status(201);
+
       chai.request(server)
       .get('/api/v1/pitcher_data/id/1')
       .end((err, response) => {
@@ -698,143 +837,60 @@ describe('pitcher_data routes', () => {
         response.should.have.status(200);
         response.should.be.json;
         response.body[0].should.be.a('object');
-        response.body.length.should.equal(1);
-        done();
+        response.body[0].name.should.equal('Roy Hobbs');
+      done();
       });
     });
+  });
 
-    it('should not get a specific pitcher with bad id info', (done) => {
-      chai.request(server)
-      .get('/api/v1/pitcher_data/id/10000')
-      .end((err, response) => {
-
-        response.should.have.status(404);
-        response.body.should.be.a('object');
-        response.body.error.should.equal('No pitchers found with id of 10000')
-        done();
-      });
-    });
-
-    it('should post a pitcher', (done) => {
-
-      let newPersonBody = {
-        "newPerson": {
-          "id": 29,
-          "career": "1984-2017",
-          "induction_method": "BBWAA",
-          "name": "Ricky Vaughn",
-          "primary_team": "Atlanta Braves",
-          "vote_percentage": "88",
-          "year_inducted": "2017",
-          "team_id": "4"
+  it('should not edit a pitcher if the id does not exist', (done) => {
+    let newUpdate = {
+      "pitcher": {
+        "name": "Roy Hobbs"
         },
-          token: authToken
-      }
-      chai.request(server)
-      .post('/api/v1/inducted_people')
-      .send(newPersonBody)
-      .end((err, response) => {
-        let newPitcherBody = {
-          "newPitcher": {
-            "id": 11,
-            "name": "Ricky Vaughn",
-            "games": "189",
-            "starts": "103",
-            "wins": "46",
-            "losses": "40",
-            "era": "3.76",
-            "strikeouts": "613",
-            "walks": "325",
-            "induction_id": 29
-          },
-            token: authToken
-        }
-        chai.request(server)
-        .post('/api/v1/pitcher_data')
-        .send(newPitcherBody)
-        .end((err, response) => {
-          response.should.have.status(201);
-          response.body.id.should.equal(11)
-          done();
-        });
+        token: authToken
+    }
+    chai.request(server)
+    .put('/api/v1/pitcher_data/id/100000')
+    .send(newUpdate)
+    .end((err, response) => {
+
+      response.should.have.status(422);
+      done();
       });
   });
 
-    it('should edit a pitcher', (done) => {
-      let newUpdate = {
-        "pitcher": {
-          "name": "Roy Hobbs"
-          },
-          token: authToken
-      }
-      chai.request(server)
-      .put('/api/v1/pitcher_data/id/1')
-      .send(newUpdate)
-      .end((err, response) => {
+  it('should delete a pitcher', (done) => {
+    chai.request(server)
+    .get('/api/v1/pitcher_data')
+    .end((err, response) => {
 
-        response.should.have.status(201);
+      response.body.length.should.equal(10);
+
+      chai.request(server)
+      .delete('/api/v1/pitcher_data/delete/1')
+      .send({ token: authToken })
+      .end((error, response) => {
+        response.should.have.status(204);
 
         chai.request(server)
-        .get('/api/v1/pitcher_data/id/1')
+        .get('/api/v1/pitcher_data')
         .end((err, response) => {
 
-          response.should.have.status(200);
-          response.should.be.json;
-          response.body[0].should.be.a('object');
-          response.body[0].name.should.equal('Roy Hobbs');
-        done();
-        });
+          response.body.length.should.equal(9);
+        })
+      done();
       });
     });
+  });
 
-    it('should not edit a pitcher if the id does not exist', (done) => {
-      let newUpdate = {
-        "pitcher": {
-          "name": "Roy Hobbs"
-          },
-          token: authToken
-      }
-      chai.request(server)
-      .put('/api/v1/pitcher_data/id/100000')
-      .send(newUpdate)
-      .end((err, response) => {
-
-        response.should.have.status(422);
-        done();
-        });
-    });
-
-    it('should delete a pitcher', (done) => {
-      chai.request(server)
-      .get('/api/v1/pitcher_data')
-      .end((err, response) => {
-
-        response.body.length.should.equal(10);
-
-        chai.request(server)
-        .delete('/api/v1/pitcher_data/delete/1')
-        .send({ token: authToken })
-        .end((error, response) => {
-          response.should.have.status(204);
-
-          chai.request(server)
-          .get('/api/v1/pitcher_data')
-          .end((err, response) => {
-
-            response.body.length.should.equal(9);
-          })
-        done();
-        });
-      });
-    });
-
-    it('should not delete a pitcher w/o a correct token', (done) => {
-      chai.request(server)
-        .delete('/api/v1/pitcher_data/delete/1')
-        .send({ token: '42' })
-        .end((error, response) => {
-          response.should.have.status(403);
-        done()
-      })
-    });
+  it('should not delete a pitcher w/o a correct token', (done) => {
+    chai.request(server)
+      .delete('/api/v1/pitcher_data/delete/1')
+      .send({ token: '42' })
+      .end((error, response) => {
+        response.should.have.status(403);
+      done()
+    })
+  });
 });
